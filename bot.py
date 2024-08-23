18dba93280d5abd3111a1a3327dd59738f0d3889
18372738 <133884450+18372738@users.noreply.github.com>
2024-08-23 17:23:46 +0300

    improved the code

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import requests
 import telegram
 from environs import Env
+from time import sleep
 
 
-def verification_information(devman_token, telegram_token, chat_id):
+def get_and_send_result_inspection(devman_token, telegram_token, chat_id):
     headers = {
         "Authorization": f"Token {devman_token}",
     }
@@ -11,21 +12,29 @@ def verification_information(devman_token, telegram_token, chat_id):
     params = {
         "timestamp" : None,
     }
-    response = requests.get(url, headers=headers, params=params, timeout=120)
-    response.raise_for_status()
-    new_attempts = response.json()
-    if new_attempts['status'] == 'found':
-        params['timestamp'] = new_attempts['last_attempt_timestamp']
-        attempts = new_attempts['new_attempts'][0]
-        message = f"Преподаватель проверил работу «{attempts['lesson_title']}» {attempts['lesson_url']}.\n"
-        if attempts['is_negative'] == True:
-            message += "К сожалению в работе нашлись ошибки"
-        else:
-            message += "Преподавтелю все понравилось, можете приступать к следующему уроку"
-        bot = telegram.Bot(token=telegram_token)
-        bot.send_message(chat_id=chat_id, text=message)
-    else:
-        params['timestamp'] = new_attempts['timestamp_to_request']
+    while True:
+        try:
+            response = requests.get(url, headers=headers, params=params, timeout=120)
+            response.raise_for_status()
+            new_attempts = response.json()
+            if new_attempts['status'] == 'found':
+                params['timestamp'] = new_attempts['last_attempt_timestamp']
+                attempt = new_attempts['new_attempts'][0]
+                message = f"Преподаватель проверил работу «{attempt['lesson_title']}» {attempt['lesson_url']}.\n"
+                if attempt['is_negative']:
+                    message += "К сожалению в работе нашлись ошибки"
+                else:
+                    message += "Преподавтелю все понравилось, можете приступать к следующему уроку"
+                bot = telegram.Bot(token=telegram_token)
+                bot.send_message(chat_id=chat_id, text=message)
+            else:
+                params['timestamp'] = new_attempts['timestamp_to_request']
+        except requests.exceptions.ReadTimeout:
+            print("Ожидание ответа сервера")
+            continue
+        except requests.exceptions.ConnectionError:
+            print("Проверьте интернет соединение")
+            time.sleep(10)
 
 
 def main():
@@ -34,14 +43,7 @@ def main():
     devman_token = env.str('DEVMAN_TOKEN')
     telegram_token = env.str('TELEGRAM_TOKEN')
     chat_id = env.str('CHAT_ID')
-
-    while True:
-        try:
-            verification_information(devman_token, telegram_token, chat_id)
-        except requests.exceptions.ReadTimeout:
-            print("Ожидание ответа сервера")
-        except requests.exceptions.ConnectionError:
-            print("Проверьте интернет соединение")
+    get_and_send_result_inspection(devman_token, telegram_token, chat_id)
 
 
 if __name__ == '__main__':
