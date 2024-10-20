081a49e9a30476bd9c915481d3fbb0e29ff17dd8
18372738 <133884450+18372738@users.noreply.github.com>
2024-10-20 21:54:52 +0300

    Added logging

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
+import logging
 import requests
 import telegram
 from environs import Env
 from time import sleep
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 
 
 def send_message(attempt, telegram_token, chat_id):
@@ -11,6 +17,7 @@ def send_message(attempt, telegram_token, chat_id):
     else:
         message += "Преподавтелю все понравилось, можете приступать к следующему уроку"
     bot = telegram.Bot(token=telegram_token)
+    logging.info(f"Отправка сообщения о проверке урока {attempt['lesson_title']}")
     bot.send_message(chat_id=chat_id, text=message)
 
 
@@ -29,6 +36,9 @@ def main():
     params = {
         "timestamp" : None,
     }
+
+    logging.info("Бот запущен и ожидает проверок...")
+
     while True:
         try:
             response = requests.get(url, headers=headers, params=params, timeout=120)
@@ -37,13 +47,14 @@ def main():
             if new_attempts['status'] == 'found':
                 params['timestamp'] = new_attempts['last_attempt_timestamp']
                 attempt = new_attempts['new_attempts'][0]
+                logging.info(f"Найдена новая проверка: {attempt['lesson_title']}")
                 send_message(attempt, telegram_token, chat_id)
             else:
                 params['timestamp'] = new_attempts['timestamp_to_request']
         except requests.exceptions.ReadTimeout:
             continue
         except requests.exceptions.ConnectionError:
-            print("Проверьте интернет соединение")
+            logging.error(f"Произошла ошибка: {e}")
             time.sleep(10)
 
 
